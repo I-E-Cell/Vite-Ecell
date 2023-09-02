fe5df67e1781d614042d35d565d27067460421c7
Gulshan <gulshansinghhhh@gmail.com>
2023-09-03 03:21:55 +0530

    addded carousel

--- src/Components/Initiatives.jsx
+++ src/Components/Initiatives.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import oo from '../assets/oo.png';
-import back from '../assets/Rectangle 18.png';
+import back from '../assets/Rectangle.png';
+import Carousel from './Carousell';
+
 
 
 const Initiatives = () => {
   return (
     <div>
-        <div className='flex my-auto mx-auto justify-center'>
-            <img className='w-[60px] h-[60px]' src={oo} alt="" />
-            <h1 className='my-auto text-[#935CFF] text-5xl  font-bold'>UR INITIATIVES</h1>
+        <div className='flex my-auto mx-auto  justify-center '>
+            <img className='w-[40px] h-[40px] my-auto sm:w-[60px] sm:h-[60px]' src={oo} alt="" />
+            <h1 className='my-auto text-[#935CFF] text-4xl sm:text-5xl  py-20 font-bold'>UR INITIATIVES</h1>
         </div>
-        <div>
-            <img className='py-10 w-[100%] h-[60em]' src={back} alt="" />
+        <div className="py-[10%] pb-10 init">
+           
+            <Carousel />
             
         </div>
     </div>
