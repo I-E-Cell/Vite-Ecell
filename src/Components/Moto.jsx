f942e84edc320d2b5bbd3ebfef6d349ec0607662
Gulshan <gulshansinghhhh@gmail.com>
2023-11-14 01:35:35 +0530

    fixed bugs

--- src/Components/Moto.jsx
+++ src/Components/Moto.jsx
@@ -6,16 +6,16 @@ import { motion } from 'framer-motion'
 
 const Moto = () => {
     return (
-        <div className='bg-[#0F1322] moto md:-mt-6 h-[34.5em] md:h-[18em] w-[100%] '>
+        <div className='bg-[#0F1322] moto md:-mt-6 h-[34.5em] md:h-[18em] w-[94%] mx-auto relative '>
             <div className='flex  top-10 flex-col md:flex-row gap-28 md:gap-16 gap justify-center relative mx-auto'>
-                <div className='w-[21em] relative mx-auto md:mx-0 md:w-[27em] lg:w-[30em] h-[10em] rounded-2xl bg-[#D2AD93]'>
+                <div className='w-[21em] relative mx-auto md:mx-0 md:w-[28em] lg:w-[35em] h-[10em] rounded-2xl bg-[#D2AD93]'>
 
-                    <div className='absolute flex flex-row gap-2 left-[72%] md:left-[76%] top-1 justify-end'>
+                    <div className='absolute flex flex-row gap-2 left-[72%] md:left-[76%] top-2 justify-end'>
                         <img className='w-9 h-6' src={ait} alt="" />
                         <img className='w-10 h-6' src={ecell_logo} alt="" />
                     </div>
 
-                    <div className='absolute mx-[3.1em] md:mx-[3.5em]  top-9 lg:mx-[4.6em] font-semibold z-10 text-4xl'>
+                    <div className='absolute mx-[3.1em] md:mx-[3.5em]  top-9 lg:mx-[6em] font-semibold z-10 text-4xl'>
                         <motion.div
                             initial={{ opacity: 0, x: -60 }}
                             whileInView={{ opacity: 1, x: 0 }}
@@ -41,7 +41,7 @@ const Moto = () => {
                             </h1>
                         </motion.div>
 
-                        <div className='w-[32%] h-[40%] bg-[#D2AD93] relative top-[1.3%]  md:top-[12.3%]  -skew-x-12 left-[80%]'>
+                        <div className='w-[32%] h-[40%] bg-[#D2AD93] relative top-[1.3%] md:top-[14%] lg:top-[25%]  -skew-x-12 left-[80%]'>
 
                         </div>
                     </div>
@@ -49,13 +49,13 @@ const Moto = () => {
 
                     </div>
                 </div>
-                <div className='w-[21em] relative top-12 mx-auto md:mx-0 md:w-[27em] lg:w-[30em] h-[10em] rounded-2xl bg-[#A1A6DC]'>
+                <div className='w-[21em] relative top-12 mx-auto md:mx-0 md:w-[28em] lg:w-[35em] h-[10em] rounded-2xl bg-[#A1A6DC]'>
                     <div className='w-[76%] relative left-[24%] -top-12 h-[10em] rounded-2xl bg-[#A1A6DC]'>
-                        <div className='absolute flex flex-row gap-2 left-[63%]  md:left-[70%] top-1 justify-end'>
+                        <div className='absolute flex flex-row gap-2 left-[63%]  md:left-[70%] top-2 justify-end'>
                             <img className='w-9 h-6' src={ait} alt="" />
                             <img className='w-10 h-6' src={ecell_logo} alt="" />
                         </div>
-                        <div className='absolute top-9 mx-[0.7em] md:mx-[1em]  lg:mx-[2em] font-semibold z-10 text-4xl'>
+                        <div className='absolute top-9 mx-[0.7em] md:mx-[1em]  lg:mx-[2.5em] font-semibold z-10 text-4xl'>
                             <motion.div
                                 initial={{ opacity: 0, x: 60 }}
                                 whileInView={{ opacity: 1, x: 0 }}
@@ -69,7 +69,7 @@ const Moto = () => {
 
                             </div>
                         </div>
-                        <div className='w-[32%] h-[40%] bg-[#A1A6DC] relative top-[13px]   -skew-x-12 -left-[26px]'>
+                        <div className='w-[32%] h-[40%] bg-[#A1A6DC] relative top-[15px]   -skew-x-12 -left-[36px]'>
 
                         </div>
                         <div className='relative min-w-[115%] text-sm -left-[22%] z-10 top-7  text-center'>
@@ -87,7 +87,7 @@ const Moto = () => {
                         </div>
                     </div>
 
-                    <div className='w-[28%] -top-[144.7%] -left-[13.5px] md:-left-[18px] relative  z-10  h-[4.5em] border-[13px] border-[#0F1322] rounded-full bg-[#A1A6DC]'>
+                    <div className='w-[28%] -top-[144.9%] -left-[13.5px] md:-left-[3.85%] lg:-left-[4%] relative  z-10  h-[4.5em] border-[13px] border-[#0F1322] rounded-full bg-[#A1A6DC]'>
 
                     </div>
                 </div>
